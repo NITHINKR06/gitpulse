fdc7563b04f08679b3168d18f50f75113d09bc06
NITHINKR06 <nithinpoojari606@gmail.com>
2026-05-21 21:05:21 +0530

    Initial commit Spring Boot project using IntelliJ IDEA

---
+++ src/main/java/com/gitpulse/gitpulse/GitpulseApplication.java
@@ -0,0 +1,13 @@
+package com.gitpulse.gitpulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitpulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitpulseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitpulse/gitpulse/GitpulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitpulse.gitpulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitpulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
